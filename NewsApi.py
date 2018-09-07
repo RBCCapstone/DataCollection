5452d1215df66ecc5f66fc11945b453d083a7625
Jessie Diep <jessiediep.1@gmail.com>
2018-09-07 17:38:34 -0400

    URL Query Updated and Excel Writer Error Fixed
    URL now uses everything endpoint, sources classified as "business and
    English" news and queried terms can be adjusted and sent to function.
    Excel now writes to csv file without error of writing empty pages.

--- NewsApi.py
+++ NewsApi.py
@@ -11,20 +11,49 @@ import os
 #import API key (environment variable)
 newsapiKey = os.environ['NEWSAPI_KEY']
 
-def NewsFromBBC(j):
+def NewsFromBBC(querylist):
   
-    # BBC news api
-    for i in range(1,j):
-        main_url = " https://newsapi.org/v2/everything?sources=bbc-news&pageSize=100&page=" + str(i) + "&apiKey=" + newsapiKey  
-        # fetching data in json format
-             
-        articlesToCSV(main_url,i)
+    completequery = ""
 
+    #Create a string to query the url        
+    for i in range(len(querylist)): #defaults at index 0  
+        #Create a string to query the url     
+        if i < len(querylist)-1:
+            completequery += querylist[i]
+            completequery += " OR "
+        else:
+            completequery += querylist[i]
+        
+    print(completequery)
+    
+    
+        
+    #Find the first page
+    main_url = " https://newsapi.org/v2/everything?q=(" + completequery + ")&sources=bloomberg,business-insider,cnbc,fortune,financial-times,financial-post,the-economist,\
+    the-wall-street-journal&pageSize=100&page=1&apiKey=" + newsapiKey  
+    #print(main_url)
+    
+    # fetching data in json format
+    open_bbc_page = requests.get(main_url).json() 
+    totalResults = open_bbc_page["totalResults"]
+    #print(totalResults)
+    
+    #Write to CSV by page, until all articles in URL are written
+    j = 1
+    while int(totalResults) > 0:
+        articlesToCSV(main_url, j)
+        j = j + 1
+        main_url = " https://newsapi.org/v2/everything?q=(" + completequery + ")&sources=bloomberg,business-insider,cnbc,fortune,financial-times,financial-post,the-economist,\
+        the-wall-street-journal&pageSize=100&page=" + str(j) + "&apiKey=" + newsapiKey  
+        totalResults = totalResults - 100 
+        #print(totalResults)
+        #print(j)
+            
 def articlesToCSV(main_url, k):
     # getting all articles in a string article
     open_bbc_page = requests.get(main_url).json()  
     article = open_bbc_page["articles"]
-
+    
     # empty list which will contain all trending news
     titles = []
     description = []
@@ -36,7 +65,7 @@ def articlesToCSV(main_url, k):
         url.append(ar["url"])        
 
              # writing all trending news to csv        
-    with open('articles.csv', 'a', newline='') as f:
+    with open('articles.csv', 'a', newline='',encoding='utf-8') as f:
         articlewriter = csv.writer(f, delimiter = ',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         for i in range(len(titles)):
             s = (((k-1)*100) + i + 1, titles[i], description[i], url[i] )
@@ -47,5 +76,7 @@ def articlesToCSV(main_url, k):
 # Driver Code
 if __name__ == '__main__': 
     # function call
-    NewsFromBBC(11) 
+    #querylist = ["Amazon", "Walmart", "Home Depot", "Comcast", "Disney", "Netflix", "McDonalds", "McDonald", "McDonald's", "Costco", "Lowe", "Lowe's", "Twenty-First Century", "Century Fox", "Starbucks", "Charter Communications", "Chart Communication", "TJX", "American Tower", "Simon Property", "Las Vegas Sands", "Crown Castle", "Target", "Carnival", "Marriott", "Sherwin-Williams", "Prologis"]
+    querylisttemp = ["Marriott","tjx","Disney"]
+    NewsFromBBC(querylisttemp) 
 
