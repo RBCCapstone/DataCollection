642813b12b827a7c08a3d5f16025ec6b45cc405b
Jessie Diep <jessiediep.1@gmail.com>
2018-10-03 15:24:09 -0400

    Starting Classification

---
+++ Scripts/Linear classifier.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  3 14:47:19 2018
+
+@author: Legendary
+"""
+
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+import sklearn
+
+from sklearn.datasets import #insert name of table here
+
+ 
+#sort data by date column
+#for each day in date
+    #
